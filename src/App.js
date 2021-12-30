1541b6fc4c85a553ca63fc30b1d7f1b97947c023
pionono <jjrr1984@gmail.com>
2021-12-30 12:55:27 +0100

    - Adding different url for proxy - Using fake data if we don't get json object

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Numbers from './Numbers';
 import Prices from './Prices';
 import { useEffect } from 'react';
 import axios from 'axios';
+import data from "./data.js";
 
 function App() {
   useEffect(() => {
@@ -11,6 +12,10 @@ function App() {
       .then(function (response) {
         // handle success
         console.log(response);
+        if (response.headers["content-type"] !== "application/json") {
+          //  We're not getting json object, so we get fake data
+          response = data;
+        }
       })
       .catch(function (error) {
         // handle error
---
+++ src/data.js
@@ -0,0 +1,121 @@
+export default {
+    "last": {
+        "nr": 510,
+        "currency": "EUR",
+        "date": {
+            "full": "Die Lottozahlen vom Freitag, den 24.12.2021",
+            "day": 24,
+            "month": 12,
+            "year": 2021,
+            "hour": 21,
+            "minute": 0,
+            "dayOfWeek": "Freitag"
+        },
+        "closingDate": "24.12.2021, 19:00",
+        "lateClosingDate": "24.12.2021, 20:10",
+        "drawingDate": "24.12.2021, 21:00",
+        "numbers": [
+            3,
+            17,
+            21,
+            35,
+            42
+        ],
+        "euroNumbers": [
+            8,
+            9
+        ],
+        "jackpot": "10",
+        "marketingJackpot": "10",
+        "specialMarketingJackpot": "10",
+        "climbedSince": 55,
+        "Winners": 906174,
+        "odds": {
+            "rank0": {
+                "winners": 0,
+                "specialPrize": 0,
+                "prize": 0
+            },
+            "rank1": {
+                "winners": 1,
+                "specialPrize": 0,
+                "prize": 1266404300
+            },
+            "rank2": {
+                "winners": 9,
+                "specialPrize": 0,
+                "prize": 22507530
+            },
+            "rank3": {
+                "winners": 24,
+                "specialPrize": 0,
+                "prize": 2978930
+            },
+            "rank8": {
+                "winners": 28322,
+                "specialPrize": 0,
+                "prize": 2600
+            },
+            "rank9": {
+                "winners": 45513,
+                "specialPrize": 0,
+                "prize": 1570
+            },
+            "rank10": {
+                "winners": 75928,
+                "specialPrize": 0,
+                "prize": 1340
+            },
+            "rank4": {
+                "winners": 58,
+                "specialPrize": 0,
+                "prize": 410880
+            },
+            "rank5": {
+                "winners": 1285,
+                "specialPrize": 0,
+                "prize": 16690
+            },
+            "rank6": {
+                "winners": 2161,
+                "specialPrize": 0,
+                "prize": 7710
+            },
+            "rank11": {
+                "winners": 149274,
+                "specialPrize": 0,
+                "prize": 1240
+            },
+            "rank7": {
+                "winners": 1989,
+                "specialPrize": 0,
+                "prize": 7180
+            },
+            "rank12": {
+                "winners": 601610,
+                "specialPrize": 0,
+                "prize": 750
+            }
+        }
+    },
+    "next": {
+        "nr": 511,
+        "currency": "EUR",
+        "date": {
+            "full": "Die Lottozahlen vom Freitag, den 31.12.2021",
+            "day": 31,
+            "month": 12,
+            "year": 2021,
+            "hour": 21,
+            "minute": 0,
+            "dayOfWeek": "Freitag"
+        },
+        "closingDate": "31.12.2021, 19:00",
+        "lateClosingDate": "31.12.2021, 20:10",
+        "drawingDate": "31.12.2021, 21:00",
+        "jackpot": "10",
+        "marketingJackpot": "10",
+        "specialMarketingJackpot": "10",
+        "climbedSince": 55
+    }
+}
