15ce3f07583923c7f3a483e1365eed0915409b1e
pionono <jjrr1984@gmail.com>
2022-01-04 20:43:05 +0100

    - Completing gitignore - Setting babel - Completing package.json - Completing webpack settings - Completing index.html - Using right style assets path

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    devServer: {
+        proxy: {
+            '/api': 'https://www.lottoland.com',
+        }
+    }
+};
